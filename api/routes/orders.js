7074a94ce702fa43c2148bce0b8743387d98da87
Petra Vos <petravos70@gmail.com>
2018-01-02 14:46:12 +0100

    refactor: modified files

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -8,8 +8,13 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
-	res.status(200).json({
-		message: 'Create orders'
+	const order = {
+		productId: req.body.productId,
+		quantity: req.body.quantity
+	};
+	res.status(201).json({
+		message: 'Create orders',
+		order: order
 	});
 });
 
