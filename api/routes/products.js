800743987ae9821a6c8cf6f62a7aa2167197f6fb
Petra Vos <petravos70@gmail.com>
2018-01-02 14:46:59 +0100

    refactor: modified file

--- api/routes/products.js
+++ api/routes/products.js
@@ -8,8 +8,13 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
+	const product = {
+		name: req.body.name,
+		price: req.body.price
+	};
 	res.status(200).json({
-		message: 'handling POST requests'
+		message: 'handling POST requests',
+		createdProduct: product
 	});
 });
 
