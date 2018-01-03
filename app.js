b15db55b33947fa51e7beeac88a4baa5bee27b96
Petra Vos <petravos70@gmail.com>
2018-01-03 15:10:58 +0100

    refactor: modified files

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
@@ -7,25 +8,23 @@ const mongoose = require('mongoose');
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
-mongoose.connect('mongodb://localhost:27017/node-rest-shop', {
-	useMongoClient: true
-});
+mongoose.connect('mongodb://localhost:27017/node-rest-shop');
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-	res.header('Acces-Control-Alllow-Origin', '*');
-	res.header(
-		'Acces-Control-Allow-Headers',
-		'origin, x-Requested-With, Content-Type, Accept, Authorization'
-	);
-	if (req.method === 'OPTIONS') {
-		res.header('Acces-Control-Allow-Methods', 'Put, POST, PATCH, DELETE, GET');
-		return res.status(200).json({});
-	}
-	next();
+  res.header('Acces-Control-Alllow-Origin', '*');
+  res.header(
+    'Acces-Control-Allow-Headers',
+    'origin, x-Requested-With, Content-Type, Accept, Authorization'
+  );
+  if (req.method === 'OPTIONS') {
+    res.header('Acces-Control-Allow-Methods', 'Put, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
 });
 
 // routes which handles requests
@@ -33,18 +32,18 @@ app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
 app.use((req, res, next) => {
-	const error = new Error('Not found');
-	error.status = 404;
-	next(error);
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
 });
 
 app.use((error, req, res, next) => {
-	res.status(error.status || 500);
-	res.json({
-		error: {
-			message: error.message
-		}
-	});
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
 });
 
 module.exports = app;
