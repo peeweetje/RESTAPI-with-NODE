396a57ead623b3881ac54f1cb922c2fed5d7899b
Petra Vos <petravos70@gmail.com>
2018-01-03 15:08:50 +0100

    refactor: added error handling to file

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,35 +1,36 @@
 const express = require('express');
+
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-	res.status(200).json({
-		message: 'get orders'
-	});
+  res.status(200).json({
+    message: 'get orders',
+  });
 });
 
 router.post('/', (req, res, next) => {
-	const order = {
-		productId: req.body.productId,
-		quantity: req.body.quantity
-	};
-	res.status(201).json({
-		message: 'Create orders',
-		order: order
-	});
+  const order = {
+    productId: req.body.productId,
+    quantity: req.body.quantity,
+  };
+  res.status(201).json({
+    message: 'Create orders',
+    order,
+  });
 });
 
 router.get('/:orderId', (req, res, next) => {
-	res.status(200).json({
-		message: 'Id orders',
-		orderId: req.params.orderId
-	});
+  res.status(200).json({
+    message: 'Id orders',
+    orderId: req.params.orderId,
+  });
 });
 
 router.delete('/:orderId', (req, res, next) => {
-	res.status(200).json({
-		message: 'delete orders',
-		orderId: req.params.orderId
-	});
+  res.status(200).json({
+    message: 'delete orders',
+    orderId: req.params.orderId,
+  });
 });
 
 module.exports = router;
