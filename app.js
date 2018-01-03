323ba18d3c5b07980697ffa228a8f5db79183caa
Petra Vos <petravos70@gmail.com>
2018-01-03 11:28:21 +0100

    refactor: added mongo connection to file

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+mongoose.connect('mongodb://localhost:27017/node-rest-shop', {
+	useMongoClient: true
+});
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
