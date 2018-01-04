836b4ebd75d838fcc044b57469f0a40ddb5d14e1
Petra Vos <petravos70@gmail.com>
2018-01-04 15:50:52 +0100

    refactor: modified file

--- api/routes/products.js
+++ api/routes/products.js
@@ -7,10 +7,22 @@ const Product = require('../models/products');
 
 router.get('/', (req, res, next) => {
   Product.find()
+    .select('name price _id')
     .exec()
     .then((docs) => {
-      console.log(docs);
-      res.status(200).json(docs);
+      const response = {
+        count: docs.length,
+        products: docs.map(doc => ({
+          name: doc.name,
+          price: doc.price,
+          _id: doc._id,
+          request: {
+            type: 'GET',
+            url: `http://localhost:3000/products/${doc._id}`,
+          },
+        })),
+      };
+      res.status(200).json(response);
     })
     .catch((err) => {
       console.log(err);
@@ -31,8 +43,16 @@ router.post('/', (req, res, next) => {
     .then((result) => {
       console.log(result);
       res.status(201).json({
-        message: 'handling POST requests',
-        createdProduct: result,
+        message: 'created products',
+        createdProduct: {
+          name: result.name,
+          price: result.price,
+          _id: result._id,
+          request: {
+            type: 'GET',
+            url: `http://localhost:3000/products/${result._id}`,
+          },
+        },
       });
     })
     .catch((err) => {
@@ -46,11 +66,19 @@ router.post('/', (req, res, next) => {
 router.get('/:productId', (req, res, next) => {
   const id = req.params.productId;
   Product.findById(id)
+    .select('name price _id')
     .exec()
     .then((doc) => {
       console.log('From Database', doc);
       if (doc) {
-        res.status(200).json(doc);
+        res.status(200).json({
+          product: doc,
+          request: {
+            type: 'GET',
+            description: 'Get all products',
+            url: 'http://localhost:3000/products',
+          },
+        });
       } else {
         res.status(404).json({
           message: 'no valid entry found for Id',
@@ -72,8 +100,13 @@ router.patch('/:productId', (req, res, next) => {
   Product.update({ _id: id }, { $set: updateOps })
     .exec()
     .then((result) => {
-      console.log(result);
-      res.status(200).json(result);
+      res.status(200).json({
+        message: 'Product Updated',
+        request: {
+          type: 'GET',
+          url: `http://localhost:3000/products/${id}`,
+        },
+      });
     })
     .catch((err) => {
       console.log(err);
@@ -88,7 +121,14 @@ router.delete('/:productId', (req, res, next) => {
   Product.remove({ _id: id })
     .exec()
     .then((result) => {
-      res.status(200).json(result);
+      res.status(200).json({
+        message: 'Product deleted',
+        request: {
+          type: 'POST',
+          url: 'http://localhost:3000/products',
+          body: { name: 'String', price: 'Number' },
+        },
+      });
     })
     .catch((err) => {
       res.status(500).json({
