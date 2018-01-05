09507f70579c69f39b3a83e6caf8105cfbf3c561
Petra Vos <petravos70@gmail.com>
2018-01-05 16:55:13 +0100

    refactor: modified file

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,22 +1,52 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const router = express.Router();
 
+const Order = require('../models/orders');
+const Product = require('../models/products');
+
 router.get('/', (req, res, next) => {
-  res.status(200).json({
-    message: 'get orders',
-  });
+  Order.find()
+    .select('product quantity _id')
+    .exec()
+    .then((docs) => {
+      res.status(200).json({
+        count: docs.length,
+        orders: docs.map(doc => ({
+          _id: doc._id,
+          product: doc.product,
+          quantity: doc.quantity,
+          request: {
+            type: 'GET',
+            url: `http://localhost:3000/orders/${doc._id}`,
+          },
+        })),
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: err,
+      });
+    });
 });
 
 router.post('/', (req, res, next) => {
-  const order = {
-    productId: req.body.productId,
+  // making sure a productId is valid
+  Product.findById(req.body.productId)
+    .then((product) => {})
+    .catch((err) => {
+      res.status(500).json({
+        message: 'ProductId not found',
+        error: err,
+      });
+    });
+  const order = new Order({
+    _id: mongoose.Types.ObjectId(),
     quantity: req.body.quantity,
-  };
-  res.status(201).json({
-    message: 'Create orders',
-    order,
+    product: req.body.productId,
   });
+  return order.save();
 });
 
 router.get('/:orderId', (req, res, next) => {
