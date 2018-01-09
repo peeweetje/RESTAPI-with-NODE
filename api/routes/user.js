8d57cf43b4db882a4e448f60782de8a1f14be9f6
Petra Vos <petravos70@gmail.com>
2018-01-09 17:03:17 +0100

    refactor: added route user to file

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,16 +1,39 @@
 const express = require('express');
 const mongoose = require('mongoose');
+
 const router = express.Router();
+const bcrypt = require('bcrypt');
 
 const User = require('../models/user');
 
 router.post('/signup', (req, res, next) => {
-   const user = new user({
-    _id: new mongoose.Types.ObjectId(),
-    email: req.body.email,
-    password: req.body.password,
-   });
-})
-
+    bcrypt.hash(req.body.password, 10, (err, hash) => {
+        if (err) {
+          return res.status(500).json({
+            error: err,
+          });
+        } else {
+            const user = new user({
+                _id: new mongoose.Types.ObjectId(),
+                email: req.body.email,
+                password: hash
+              });
+              user
+              .save()
+              .then(result =>{
+                  console.log(result);
+                  res.status(201).json({
+                      message: 'User created'
+                  })
+              })
+              .catch(err => {
+                 console.log(err);
+                 res.status(500).json({
+                     error: err
+                 })
+              });
+           }
+      }),
+});
 
 module.exports = router;
